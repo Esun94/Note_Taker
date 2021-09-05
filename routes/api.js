338f378393e8176343a8f4ba7e2b5bf58962a3f4
Eric Sun <ericmsun@gmail.com>
2021-09-04 23:04:55 -0400

    saving progress on delete()

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
-
+const {v4 : uuidv4} = require('uuid')
 const fs = require('fs');
-router.get('/notes', (req, res) => {
+
+router.get('/notes/', (req, res) => {
     const notes = fs.readFileSync('db.json', 'utf8');
     const noteArry = JSON.parse(notes)
     res.json(noteArry)
@@ -13,7 +14,7 @@ router.post('/notes', (req, res) => {
     const noteObj = {
         title: req.body.title,
         text: req.body.text,
-        id: 1
+        id: uuidv4()
     }
     const newNoteArry = noteArry.concat(noteObj);
     fs.writeFileSync("db.json", JSON.stringify(newNoteArry))
@@ -21,12 +22,25 @@ router.post('/notes', (req, res) => {
     res.redirect('/notes');
 })
 
-
-
-
-
-
-
+router.delete("/notes/:id", function (req, res) {
+    const noteId = JSON.parse(req.params.id)
+    console.log(noteId)
+    fs.readFile("db.json", 'utf8', function (error, notes) {
+      if (error) {
+        return console.log(error)
+      }
+      notes = JSON.parse(notes)
+  
+      notes = notes.filter(val => val.id !== noteId)
+  
+      fs.writeFile("db.json", JSON.stringify(notes), function (error, data) {
+        if (error) {
+          return error
+        }
+        res.json(notes)
+      })
+    })
+  })
 
 
 
